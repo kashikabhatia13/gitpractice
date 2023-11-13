94123d8b6c0c3d5c8283eba09bd593d4920b018e
Kashika Bhatia <kashikabhatialko@gmail.com>
2023-11-13 20:56:31 +0530

    Basic Login form using cpp

---
+++ Login_form.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class User{
+    private:
+        string username, password;
+    public:
+        User(string name,string pass)
+        {
+            username=name;
+            password=pass;
+        }
+};
+
+class UserManager
+{
+    private:
+        vector<User> users; 
+    public:
+        void RegisterUser(){
+            string username,password;
+            cout<< "Enter username : ";
+            cin >> username;
+            cout<< "Enter password : ";
+            cin >> password;
+            User newUser(username,password);
+            users.push_back(username,password);
+
+            cout << "\t\t User registered successfully\n";
+        }
+};
+
+main()
+{
+    
+}
